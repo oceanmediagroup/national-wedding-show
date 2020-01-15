784c37340426c11e0761f94f1c12b40297bb6cda
trykoszko <michal.trykoszko@pagepro.pl>
2020-01-15 15:22:35 +0100

    change from, to vars from env to acf

--- web/app/themes/national-wedding-show/functions.php
+++ web/app/themes/national-wedding-show/functions.php
@@ -77,14 +77,14 @@ if (function_exists('acf_add_options_page')) {
     ));
 
     acf_add_options_sub_page(array(
-        'page_title' => 'Contact form settings',
-        'menu_title' => 'Contact form settings',
+        'page_title' => 'Theme Footer Settings',
+        'menu_title' => 'Footer',
         'parent_slug' => 'theme-general-settings',
     ));
 
     acf_add_options_sub_page(array(
-        'page_title' => 'Theme Footer Settings',
-        'menu_title' => 'Footer',
+        'page_title' => 'Contact form settings',
+        'menu_title' => 'Contact form settings',
         'parent_slug' => 'theme-general-settings',
     ));
 
