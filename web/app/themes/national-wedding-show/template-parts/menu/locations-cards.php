cc89a306f88f59d878b40346a8f14b1ddb2a53dd
filip <filip@gmail.com>
2020-07-13 14:46:39 +0200

    Change styles for locations

--- web/app/themes/national-wedding-show/template-parts/menu/locations-cards.php
+++ web/app/themes/national-wedding-show/template-parts/menu/locations-cards.php
@@ -12,37 +12,37 @@
         if (have_rows('location-cards', 'option')):
             while (have_rows('location-cards', 'option')) : the_row(); ?>
 
-                <?php
+        <?php
                 $id = get_sub_field('show');
                 $url = get_the_permalink($id);
                 $title = get_the_title($id);
                 ?>
 
-                <div class="col-lg-4 locations__single-wrapper color-overlay-wrapper lazy"
-                     data-bg="url('<?php echo get_sub_field('image')['sizes']['medium_large']; ?>')">
-                    <div class="locations__single-link">
-                        <div class="row align-items-center w-100 h-100">
-                            <div class="col align-self-center">
-                                <a href="<?php echo $url ?>">
-                                    <span class="locations__single-title">
-                                        <?php echo $title ?>
-                                    </span>
-                                    <span class="locations__single-date">
-                                        <?php if (get_field('show_already_happened', $id)): ?>
-                                            <?php echo get_field('show_replacement_text', $id) ?>
-                                        <?php else: ?>
-                                            <?php echo get_field('show_dates', $id) ?>
-                                        <?php endif; ?>
-                                    </span>
-                                </a>
-                                <a href="<?php echo get_sub_field('link')['url']; ?>"
-                                   class="button--bg-coral button--bg-coral-bold locations__single-buy">BUY TICKETS</a>
-                            </div>
-                        </div>
-                        <span class="color-overlay"></span>
+        <div class="col-lg-4 locations__single-wrapper color-overlay-wrapper lazy"
+            data-bg="url('<?php echo get_sub_field('image')['sizes']['medium_large']; ?>')">
+            <div class="locations__single-link">
+                <div class="row align-items-center w-100 h-100">
+                    <div class="col align-self-center">
+                        <a href="<?php echo $url ?>">
+                            <span class="locations__single-title">
+                                <?php echo $title ?>
+                            </span>
+                            <span class="locations__single-date">
+                                <?php if (get_field('show_already_happened', $id)): ?>
+                                <?php echo get_field('show_replacement_text', $id) ?>
+                                <?php else: ?>
+                                <?php echo get_field('show_dates', $id) ?>
+                                <?php endif; ?>
+                            </span>
+                        </a>
+                        <a href="<?php echo get_sub_field('link')['url']; ?>"
+                            class="button button--new-primary-dark locations__single-buy">BUY TICKETS</a>
                     </div>
                 </div>
-            <?php endwhile;
+                <span class="color-overlay"></span>
+            </div>
+        </div>
+        <?php endwhile;
         else :
             // no rows found
         endif;
@@ -51,8 +51,7 @@
             <?php $button = get_field('highlighted_card2', 'option')['link_cta_button']; ?>
 
             <a href="<?php echo $button['external_link_checkbox'] ? $button['cta_link_external'] : $button['cta_link_internal']; ?>"
-               class="locations__single-link"
-                <?php if ($button['link_in_new_tab']) echo "target='_blank'" ?>>
+                class="locations__single-link" <?php if ($button['link_in_new_tab']) echo "target='_blank'" ?>>
                 <div class="row align-items-center w-100 h-100">
                     <div class="col align-self-center">
                         <span class="locations__single-title">
