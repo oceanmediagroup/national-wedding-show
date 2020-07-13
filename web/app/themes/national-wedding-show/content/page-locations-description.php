92501a2ab734bd7bbc392c12a7439c043ccf2684
filip <filip@gmail.com>
2020-07-13 16:22:41 +0200

    Location page changes

--- web/app/themes/national-wedding-show/content/page-locations-description.php
+++ web/app/themes/national-wedding-show/content/page-locations-description.php
@@ -1,7 +1,7 @@
 <section class="page-locations-desc">
     <div class="container">
         <div class="row">
-            <div class="col-12 page-locations-desc__text-wrapper">
+            <div class="col-12 page-locations-desc__text-wrapper text-center">
                 <div class="row">
                     <div class="col-12">
                         <h2 class="page-locations-desc__title"><?php the_field('locations_description-title'); ?></h2>
@@ -20,7 +20,7 @@
                                 $target = "target='" . $link['target'] . "'";
                             } ?>
                         <a href="<?php echo $link['url']; ?>" <?php echo $target ?>
-                            class="col-12 col-md-5 page-locations-desc__button-link button--light-coral"><?php the_field('locations_description-cta-label'); ?></a>
+                            class="page-locations-desc__button-link button button--new-primary-dark"><?php the_field('locations_description-cta-label'); ?></a>
                         <?php endif; ?>
 
                         <?php if (get_field('locations_description-cta-label-sec') && get_field('locations_description-cta-link-sec')): ?>
@@ -31,8 +31,9 @@
                                 $target = "target='" . $link['target'] . "'";
                             } ?>
                         <a href="<?php echo $link['url']; ?>" <?php echo $target ?>
-                            class="col-12 col-md-5 page-locations-desc__button-link button--light-coral"><?php the_field('locations_description-cta-label-sec'); ?></a>
-                        <?php endif; ?></div>
+                            class="page-locations-desc__button-link button button--new-primary-dark"><?php the_field('locations_description-cta-label-sec'); ?></a>
+                        <?php endif; ?>
+                    </div>
 
                 </div>
             </div>
