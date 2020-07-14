923f68ef583ea5ed144827f952559ed3c8a491ed
filip <filip@gmail.com>
2020-07-14 13:14:57 +0200

    Add static instagran component

---
+++ web/app/themes/national-wedding-show/components/instagram-feed.php
@@ -0,0 +1,33 @@
+<?php 
+    $is_home = (is_front_page() ? ' top-accent top-accent--white' : '');
+?>
+
+<section class="instagram<?php echo $is_home?>">
+
+    <div class="container">
+        <div class="row">
+            <div class="col instagram__header-col text-center">
+                <h2 class="instagram__title t-section-heading "><span class="t-underline-coral--alpha">Follow
+                        us</span>
+                </h2>
+                <a href="https://www.instagram.com/thenationalweddingshow/?hl=en"
+                    class="instagram__profile-link t-black">@nationalweddingshow</a>
+
+
+
+                <a href="https://www.instagram.com/explore/tags/nationalweddingshow/"
+                    class="instagram__circle-button button--circle-alt"><span class="t-stroked t-stroked--black">#NWS
+                        </spa></a>
+            </div>
+
+
+        </div>
+
+        <div class="row instagram__feed-row">
+            <div class="col-md-4"><img class="w-100" src="https://via.placeholder.com/300" alt=""></div>
+            <div class="col-md-4"><img class="w-100" src="https://via.placeholder.com/300" alt=""></div>
+            <div class="col-md-4"><img class="w-100" src="https://via.placeholder.com/300" alt=""></div>
+
+        </div>
+    </div>
+</section>
