cbcc1c7c0090fc0c13b743bb4597988fcd82cf61
filip <filip@gmail.com>
2020-07-15 13:59:51 +0200

    Exhibitors video - increase padding, fonts, btn

--- web/app/themes/national-wedding-show/components/exhibitors-video.php
+++ web/app/themes/national-wedding-show/components/exhibitors-video.php
@@ -3,15 +3,19 @@
  * User: Oleh
  */ ?>
 
-<section class="exhibitors-video lazy" data-bg="url('<?php $bg_img = get_field('block_background_image'); echo $bg_img['url']; ?>')">
+<section class="exhibitors-video lazy"
+	data-bg="url('<?php $bg_img = get_field('block_background_image'); echo $bg_img['url']; ?>')">
 	<div class="container">
 		<div class="row">
 			<div class="col-12 col-lg-10 video-col m-auto">
-				<h3><?php the_field('title'); ?></h3>
+				<h3 class="t-section-heading"><span
+						class="t-underline-powder--alpha"><?php the_field('title'); ?></span></h3>
 				<div class="embed-responsive embed-responsive-21by9">
-				  <iframe class="embed-responsive-item lazy" data-src="<?php the_field('video_link'); ?>?rel=0" allowfullscreen></iframe>
+					<iframe class="embed-responsive-item lazy" data-src="<?php the_field('video_link'); ?>?rel=0"
+						allowfullscreen></iframe>
 				</div>
-				<a href="<?php the_field('cta_link'); ?>"><?php the_field('cta_text'); ?></a>
+				<a class="button button--new-primary-dark"
+					href="<?php the_field('cta_link'); ?>"><?php the_field('cta_text'); ?></a>
 			</div>
 		</div>
 	</div>
