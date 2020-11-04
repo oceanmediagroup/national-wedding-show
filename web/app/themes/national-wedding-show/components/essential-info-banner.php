0ddc81830f1af8076ebd7b21c011debd1323279c
Vinod Soba <vsoba@oceanmediagroup.eu>
2020-11-04 13:03:17 +0000

    Essential Info Page Changes made underline, fix info banner and button colours

--- web/app/themes/national-wedding-show/components/essential-info-banner.php
+++ web/app/themes/national-wedding-show/components/essential-info-banner.php
@@ -6,7 +6,8 @@
  * Time: 15:38
  */ ?>
 
-<section class="info-banner" style="background-color: <?php echo get_field('ribbon_color'); ?>">
+<section class="info-banner" style="margin: 127px 0px 156px; background-color: <?php echo get_field('ribbon_color'); ?>">
+  <img data-src="/assets/img/section-accents/confetti.png" class="lazy confetti-accent" alt="" />
     <div class="container">
         <div class="row justify-content-between align-items-center">
             <div class="col-12 col-md col-xl">
@@ -19,7 +20,7 @@
                 $button = get_field('essential_cta_ribbon_cta_button'); ?>
 
                 <a href="<?php echo $button['external_link_checkbox'] ? $button['cta_link_external'] : $button['cta_link_internal']; ?>"
-                   class="info-banner__button button--light-gold button--white-mobile w-100"
+                   class="info-banner__button button--circle"
                     <?php if ($button['link_in_new_tab']) echo "target='_blank'" ?>>
                     <?php echo $button['cta_title'] ?>
                 </a>
